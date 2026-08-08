379587cee00f767c1e787d00cfeaf49c6c12a41a
Jacob Anderson <jacobdanderson@gmail.com>
2026-08-08 12:44:29 -0400

    Normalize instructional code formatting

--- DM7-Simple-Sort/main.py
+++ DM7-Simple-Sort/main.py
@@ -1,20 +1,22 @@
 import random
 
+
 def simpleSort(list):
     sortedList = []
     while len(list) > 0:
         minElem = list[0]
         for num in list:
-          if num < minElem:
-            minElem = num
+            if num < minElem:
+                minElem = num
         list.remove(minElem)
         sortedList.append(minElem)
     return sortedList
 
+
 # create list of 10 random numbers
 list = []
-for i in range(0,10):
-    list.append(random.randint(0,100))
+for i in range(0, 10):
+    list.append(random.randint(0, 100))
 
 # sort the lists
 list = simpleSort(list)
